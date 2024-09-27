252b22caa54889870f032578f3729e8c9ce27597
Athrva Deshmukh <112002659+athrvadeshmukh@users.noreply.github.com>
2024-09-27 08:17:17 +0000

    Auto commit on save

--- program2.py
+++ program2.py
@@ -4,7 +4,19 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        pass
-
-
-
+        # Dictionary to map Roman numerals to their integer values
+        roman_values = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        
+        # Variable to store the final integer value
+        total = 0
+        
+        # Traverse the Roman numeral string
+        for i in range(len(s)):
+            # If the current value is less than the next value, subtract it
+            if i + 1 < len(s) and roman_values[s[i]] < roman_values[s[i + 1]]:
+                total -= roman_values[s[i]]
+            else:
+                # Otherwise, add it to the total
+                total += roman_values[s[i]]
+        
+        return total
